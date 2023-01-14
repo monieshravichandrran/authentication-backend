79437fb45af58f4c9967562793f36a6385cb4c28
Moniesh Ravichandran <monieshravichandran@Monieshs-MacBook-Pro.local>
2023-01-14 23:16:54 +0530

    first commit

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({extended: true}));
 
+mongoose.set("strictQuery", false);
 mongoose.connect(process.env.MONGOOSE_URL,{
     useNewUrlParser: true,
     useUnifiedTopology: true
